4266f50856013a86c6cec6f4434f8da5e464849b
alvin.hhh <alvin@jennifersoft.com>
2022-07-07 16:56:25 +0900

    Partition 샘플추ㄱ

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Partition from './components/Partition';
 import Treemap from './components/Treemap';
 import Tree from './components/Tree';
 import './App.css';
@@ -6,6 +7,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Partition></Partition>
+      <Treemap></Treemap>
       <Tree></Tree>
     </div>
   );
---
+++ src/components/Partition.tsx
@@ -0,0 +1,73 @@
+import type { HierarchyNode, HierarchyRectangularNode } from 'd3-hierarchy';
+import React from 'react';
+import { hierarchy, partition } from 'd3-hierarchy';
+
+const data = {
+  name: 'A1',
+  children: [
+    {
+      name: 'B1',
+      children: [
+        {
+          name: 'C1',
+          value: 100,
+        },
+        {
+          name: 'C2',
+          value: 300,
+        },
+        {
+          name: 'C3',
+          value: 200,
+        },
+      ],
+    },
+    {
+      name: 'B2',
+      value: 200,
+    },
+  ],
+};
+
+function Partition() {
+  const padding = 10;
+  const width = 400;
+  const height = 200;
+  const root = hierarchy(data);
+  const partitionLayout = partition().size([width, height]).padding(5);
+
+  root.sum((data: any) => {
+    return data.value;
+  });
+
+  partitionLayout(root);
+
+  console.log(root.descendants());
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width + padding}
+      height={height + padding}
+    >
+      <g transform={`translate(${padding / 2}, ${padding / 2})`}>
+        {root.descendants().map((node, index) => {
+          const newNode = node as HierarchyRectangularNode<any>;
+          return (
+            <rect
+              key={index}
+              fill="white"
+              stroke="black"
+              x={newNode.x0}
+              y={newNode.y0}
+              width={newNode.x1 - newNode.x0}
+              height={newNode.y1 - newNode.y0}
+            ></rect>
+          );
+        })}
+      </g>
+    </svg>
+  );
+}
+
+export default Partition;
